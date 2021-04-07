60a2b1c287b61dd74fbb165e5b7216670d1056d1
Aryan Shah <aryanshah791@gmail.com>
2021-04-07 15:50:03 -0400

    Add landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import { Row, Col } from "react-bootstrap";
+
+function Landing() {
+  return (
+    <Row className="my-5">
+      <Col>
+        <Row>
+          <Col>
+            <h1>Welcome to GoStudy</h1>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+}
+
+export default Landing;
