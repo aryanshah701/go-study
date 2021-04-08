94a0fa1d8121357fe8c5dd3d0504c1d39746a5ce
Aryan Shah <aryanshah791@gmail.com>
2021-04-08 10:40:12 -0400

    Fix landing page closing tag bug

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,17 +1,23 @@
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from "react-bootstrap";
 
 function Landing() {
   return (
-    <Row className="my-5">
-      <Col>
-        <h1>Welcome to GoStudy!</h1>
-      </Col>
-    </Row>
     <Row>
       <Col>
-        <img class="img-fluid"
-          src="https://media.istockphoto.com/vectors/young-people-studying-together-outdoors-on-nature-background-exam-vector-id1208142883?b=1&k=6&m=1208142883&s=170667a&w=0&h=WIPSA8AX7WNvgYt8QtucnLzlXi8YhGh4G2GrM_tlBGE="
-          alt="card-image-cap">
+        <Row className="my-5">
+          <Col>
+            <h1>Welcome to GoStudy!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img
+              class="img-fluid"
+              src="https://media.istockphoto.com/vectors/young-people-studying-together-outdoors-on-nature-background-exam-vector-id1208142883?b=1&k=6&m=1208142883&s=170667a&w=0&h=WIPSA8AX7WNvgYt8QtucnLzlXi8YhGh4G2GrM_tlBGE="
+              alt="landing page"
+            ></img>
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
